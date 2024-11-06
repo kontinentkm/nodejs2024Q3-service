782e73dc955406fd6c3b517e79f25667a8de9641
Max <KontinentKM@gmail.com>
2024-11-06 15:45:40 +0300

    feat: implement artist modules

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { ArtistService } from './artist.service';
+import { CreateArtistDto } from './dto/create-artist.dto';
 
 @Controller('artist')
-export class ArtistController {}
+export class ArtistController {
+  constructor(private readonly artistService: ArtistService) {}
+
+  @Get()
+  getAllArtists() {
+    return this.artistService.getAllArtists();
+  }
+
+  @Get(':id')
+  getArtistById(@Param('id') id: string) {
+    return this.artistService.getArtistById(id);
+  }
+
+  @Post()
+  createArtist(@Body() createArtistDto: CreateArtistDto) {
+    return this.artistService.createArtist(createArtistDto);
+  }
+
+  @Put(':id')
+  updateArtist(
+    @Param('id') id: string,
+    @Body() updateArtistDto: CreateArtistDto,
+  ) {
+    return this.artistService.updateArtist(id, updateArtistDto);
+  }
+
+  @Delete(':id')
+  deleteArtist(@Param('id') id: string) {
+    return this.artistService.deleteArtist(id);
+  }
+}
--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { CreateArtistDto } from './dto/create-artist.dto';
 
 @Injectable()
-export class ArtistService {}
+export class ArtistService {
+  private artists = [];
+
+  getAllArtists() {
+    return this.artists;
+  }
+
+  getArtistById(id: string) {
+    return this.artists.find((artist) => artist.id === id);
+  }
+
+  createArtist(createArtistDto: CreateArtistDto) {
+    const newArtist = { id: 'some-uuid', ...createArtistDto };
+    this.artists.push(newArtist);
+    return newArtist;
+  }
+
+  updateArtist(id: string, updateArtistDto: CreateArtistDto) {
+    const artist = this.artists.find((artist) => artist.id === id);
+    if (artist) {
+      Object.assign(artist, updateArtistDto);
+      return artist;
+    }
+    return null;
+  }
+
+  deleteArtist(id: string) {
+    this.artists = this.artists.filter((artist) => artist.id !== id);
+  }
+}
