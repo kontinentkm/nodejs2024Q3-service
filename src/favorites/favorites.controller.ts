11cb5cb6b1129dcf2b0c20a529c00ce278adb2f7
Max <KontinentKM@gmail.com>
2024-11-06 15:52:33 +0300

    feat: implement favorites module

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
+import { FavoritesService } from './favorites.service';
 
 @Controller('favorites')
-export class FavoritesController {}
+export class FavoritesController {
+  constructor(private readonly favoritesService: FavoritesService) {}
+
+  @Get()
+  getAllFavorites() {
+    return this.favoritesService.getAllFavorites();
+  }
+
+  @Post('add')
+  addFavorite(@Body() favorite: { type: string; id: string }) {
+    return this.favoritesService.addFavorite(favorite);
+  }
+
+  @Delete(':id')
+  removeFavorite(@Param('id') id: string) {
+    return this.favoritesService.removeFavorite(id);
+  }
+}
--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class FavoritesService {}
+export class FavoritesService {
+  private favorites = [];
+
+  getAllFavorites() {
+    return this.favorites;
+  }
+
+  addFavorite(favorite: { type: string; id: string }) {
+    this.favorites.push(favorite);
+    return favorite;
+  }
+
+  removeFavorite(id: string) {
+    this.favorites = this.favorites.filter((fav) => fav.id !== id);
+  }
+}
