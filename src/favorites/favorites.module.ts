a7922ddf99f5ff938a5d7a5a3384231de020ecbb
Max <KontinentKM@gmail.com>
2024-11-23 11:51:42 +0300

    feat: 5 favorites tests passed

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { FavoritesController } from './favorites.controller';
 import { FavoritesService } from './favorites.service';
 import { PrismaService } from 'prisma/prisma.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [FavoritesController],
   providers: [FavoritesService, PrismaService],
 })
