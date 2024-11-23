206a827e38b8c0f6fb6790918ad2b3fced4036ba
Max <KontinentKM@gmail.com>
2024-11-23 11:44:28 +0300

    feat: 5 track tests passed

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -3,8 +3,10 @@ import { Module } from '@nestjs/common';
 import { TrackController } from './track.controller';
 import { TrackService } from './track.service';
 import { PrismaService } from 'prisma/prisma.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [TrackController],
   providers: [TrackService, PrismaService],
   exports: [TrackService],
